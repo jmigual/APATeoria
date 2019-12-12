d9275f21f5a21c0c41062ccd1ea59a86dff076c7
Joan <j.marce.igual@gmail.com>
2019-12-12 22:31:01 +0100

    It starts to work

--- Notes/TFM/presentations/images/src/drawing_2019_12_17.py
+++ Notes/TFM/presentations/images/src/drawing_2019_12_17.py
@@ -17,10 +17,11 @@ class SquareToCircle(Scene):
 class GangAnimation(GraphScene):
 
     CONFIG = {
-        "x_min": -1,
-        "x_max": 20,
-        "y_min": -1,
-        "y_max": 10,
+        "x_min": 0,
+        "x_max": 15,
+        "y_min": 0,
+        "y_max": 4,
+        "y_axis_height": 3,
         "x_axis_label": "Time",
         "y_axis_label": "Core"
     }
@@ -33,21 +34,36 @@ class GangAnimation(GraphScene):
             Transform(self.y_axis_label_mob, self.y_axis_label_mob.set_color(BLACK))
         )
 
-        core_usage = [[0, 10], [0, 7], [0, 7], [0, 5]]
-        self.draw_cores(core_usage)
+        gang_tasks = [[5, 3], [6, 2]]
+        colors = ["#67EB34", "#F269FF"]
+        cores = 0
 
-    def draw_cores(self, cores: List[List[int]]):
-        core = 0
-
-        for [cmin, cmax] in cores:
-            coords = self.coords_to_point(cmax - cmin, 1)
-            coords -= self.coords_to_point(0, 0)
-            width, height = coords[0], coords[1]
-            print(width, height)
-            rec = Rectangle(width=width, height=height, color=BLACK)
-            rec.move_to(self.coords_to_point((cmax - cmin)/2, core + .5))
+        for [width, height], color in zip(gang_tasks, colors):
+            point = self.coords_to_point(width, height)
+            point -= self.coords_to_point(0, 0)
+            rec = Rectangle(width=point[0], height=point[1], color=color)
+            rec.move_to(self.coords_to_point(width/2, cores + height/2))
+            rec.set_fill(color, opacity=1)
             self.play(FadeIn(rec))
-            # rec.set_coord()
-            core += 1
+            cores += height
+
+        self.wait(1)
+
+    #     core_usage = [[0, 10], [0, 7], [0, 7], [0, 5]]
+    #     self.draw_cores(core_usage)
+
+    # def draw_cores(self, cores: List[List[int]]):
+    #     core = 0
+
+    #     for [cmin, cmax] in cores:
+    #         coords = self.coords_to_point(cmax - cmin, 1)
+    #         coords -= self.coords_to_point(0, 0)
+    #         width, height = coords[0], coords[1]
+    #         print(width, height)
+    #         rec = Rectangle(width=width, height=height, color=BLACK)
+    #         rec.move_to(self.coords_to_point((cmax - cmin)/2, core + .5))
+    #         self.play(FadeIn(rec))
+    #         # rec.set_coord()
+    #         core += 1
 
 
