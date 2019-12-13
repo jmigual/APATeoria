5d26d64ddbad673ef67ee6b1094999bdcf6ae02b
Joan <j.marce.igual@gmail.com>
2019-12-13 12:48:30 +0100

    Start adding first steps of drawings

--- Notes/TFM/presentations/images/src/drawing_2019_12_17.py
+++ Notes/TFM/presentations/images/src/drawing_2019_12_17.py
@@ -1,18 +1,8 @@
-from typing import List
+from typing import List, Union
+Num = Union[int, float]
 
 from manimlib.imports import *
 
-class SquareToCircle(Scene):
-    def construct(self):
-        circle = Circle()
-        square = Square()
-        square.flip(RIGHT)
-        square.rotate(-3*TAU/8)
-        circle.set_fill(PINK, opacity=.5)
-
-        self.play(ShowCreation(square))
-        self.play(Transform(square, circle))
-        self.play(FadeOut(square))
 
 class GangAnimation(GraphScene):
 
@@ -23,47 +13,78 @@ class GangAnimation(GraphScene):
         "y_max": 4,
         "y_axis_height": 3,
         "x_axis_label": "Time",
-        "y_axis_label": "Core"
+        "y_axis_label": "Core",
     }
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.colors = []
+
     def construct(self):
+
+        self.colors = ["#67EB34", "#F269FF"]
+
         self.setup_axes(animate=True)
-        # self.play(Transform(self.x_axis_label_mob, self.x_axis_label_mob.set_fill(BLACK)))
         self.play(
-            Transform(self.x_axis_label_mob, self.x_axis_label_mob.set_color(BLACK)),
-            Transform(self.y_axis_label_mob, self.y_axis_label_mob.set_color(BLACK))
+            ApplyMethod(self.x_axis_label_mob.set_color, "#000000"),
+            ApplyMethod(self.y_axis_label_mob.set_color, "#000000")
         )
+        self.play(ApplyMethod(self.y_axis_label_mob.shift, 1.5*LEFT))
 
-        gang_tasks = [[5, 3], [6, 2]]
-        colors = ["#67EB34", "#F269FF"]
-        cores = 0
+        tasks = [[[5, 3], [7.5, 2], [15, 1]], [[6, 2], [12, 1]]]
 
-        for [width, height], color in zip(gang_tasks, colors):
-            point = self.coords_to_point(width, height)
-            point -= self.coords_to_point(0, 0)
-            rec = Rectangle(width=point[0], height=point[1], color=color)
-            rec.move_to(self.coords_to_point(width/2, cores + height/2))
-            rec.set_fill(color, opacity=1)
-            self.play(FadeIn(rec))
-            cores += height
+        for task, color in zip(tasks, self.colors):
+            main_rec = None
+            for width, height in task:
+                rec = self.create_coords_rec(width, height, color)
+                rec.move_to(self.coords_to_point(width/2, height/2))
+                if main_rec is not None:
+                    self.play(Transform(main_rec, rec))
+                else:
+                    main_rec = rec
+                    self.bring_to_back(main_rec)
+                    self.play(Write(rec))
+                self.wait(1)
+            self.play(FadeOut(main_rec))
 
-        self.wait(1)
+        # main_rec = self.show_first_gangs()
+        #
+        # task_2 = [6, 2]
+        # task_2_rec = self.create_coords_rec(task_2[0], task_2[1], self.colors[1])
+        # task_2_rec.move_to(self.coords_to_point(task_2[0]/2, 3 + task_2[1]/2))
+        # self.bring_to_back(task_2_rec)
+        # self.play(ShowCreation(task_2_rec))
 
-    #     core_usage = [[0, 10], [0, 7], [0, 7], [0, 5]]
-    #     self.draw_cores(core_usage)
+    def show_first_gangs(self) -> Rectangle:
+        gang_tasks = [[5, 3], [7.5, 2], [15, 1]]
 
-    # def draw_cores(self, cores: List[List[int]]):
-    #     core = 0
+        original_rec = None
+        main_rec = None
+        for width, height in gang_tasks:
+            rec = self.create_coords_rec(width, height, self.colors[0])
+            rec.move_to(self.coords_to_point(width/2, height/2))
+            self.bring_to_back(rec)
+            if main_rec is not None:
+                self.play(Transform(main_rec, rec))
+            else:
+                original_rec = rec.deepcopy()
+                self.remove(original_rec)
+                main_rec = rec
+                self.play(Write(rec))
 
-    #     for [cmin, cmax] in cores:
-    #         coords = self.coords_to_point(cmax - cmin, 1)
-    #         coords -= self.coords_to_point(0, 0)
-    #         width, height = coords[0], coords[1]
-    #         print(width, height)
-    #         rec = Rectangle(width=width, height=height, color=BLACK)
-    #         rec.move_to(self.coords_to_point((cmax - cmin)/2, core + .5))
-    #         self.play(FadeIn(rec))
-    #         # rec.set_coord()
-    #         core += 1
+            self.wait(2)
+
+        self.play(Transform(main_rec, original_rec))
+        return main_rec
+
+    def create_coords_rec(self, width: Num, height: Num, color: str = "#000000") -> Rectangle:
+        point = self.coords_to_point(width, height)
+        point -= self.coords_to_point(0, 0)
+        rec = Rectangle(width=point[0], height=point[1], color=color)
+        rec.set_fill(color, opacity=1.)
+        return rec
 
 
+if __name__ == "__main__":
+    module_name = os.path.basename(__file__)
+    os.system(f"manim -p -m -c white {module_name} {sys.argv[1]}")
